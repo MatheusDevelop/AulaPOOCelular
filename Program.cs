25b5107b2a126eb6212d8410e8abcaa5513236d7
Matheus Barbosa <mathvierabarbosa@gmail.com>
2020-05-27 10:02:12 -0300

    Formatando console com cores

--- Program.cs
+++ Program.cs
@@ -12,7 +12,72 @@ namespace _01
             
             Console.WriteLine("O celular ligou");
             state =true;
+            Console.WriteLine("Digite uma marca de celular");
+            string marca= Console.ReadLine();
+            Console.WriteLine("Digite a cor do celular");
+            string cor= Console.ReadLine();
+                 
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine("Marca {0} , Cor : {1}",marca,cor);
+            Console.ResetColor();
+
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("_____________________");
+            Console.WriteLine("|                   |");
+            Console.WriteLine("|                   |");
+            Console.WriteLine("|                   |");
+            Console.WriteLine("|                   |");
+            Console.WriteLine("|Hello world.       |");            
+            Console.WriteLine("|                   |");
+            Console.WriteLine("|                   |");
+            Console.WriteLine("|                   |");
+            Console.WriteLine("|                   |");
+            Console.WriteLine("|___________________|");
+            Console.WriteLine("|                   |");
+            Console.WriteLine("|___________________|");
+            Console.ResetColor();
+            inicio:
+            Console.ForegroundColor = ConsoleColor.Magenta;
             
+            Console.WriteLine("_________________________________________");
+            Console.WriteLine("Digite MENSAGEM para enviar uma mensagem");
+            Console.WriteLine("Digite LIGAR para uma ligaçao");
+            Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Digite DESLIGAR para desligar o celular");
+            Console.ResetColor();
+            switch (Console.ReadLine())
+            {
+                case "MENSAGEM":
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine("Digite uma mensagem");
+                        Mensagem(Console.ReadLine());
+                        Console.ResetColor();
+                        goto inicio;
+                        
+                        
+                        break;
+                    case "LIGAR":
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine("Digite o numero para ligaçao");
+                        Ligar(Console.ReadLine());
+                        Console.ResetColor();
+                       goto inicio;
+                        
+                        
+                        break;
+                    case "DESLIGAR":
+                        Desligar();
+                        
+                        break;
+                default:
+                        Console.WriteLine("Comando invalido");
+                        Console.Clear();
+                        goto inicio;
+                        
+                       
+                        break;    
+            }
             return true;
         }
         public string Mensagem(string msg){
@@ -61,48 +126,11 @@ namespace _01
                     break;
            };
            
-           Console.WriteLine("_____________________");
-           Console.WriteLine("|                   |");
-           Console.WriteLine("|                   |");
-           Console.WriteLine("|                   |");
-           Console.WriteLine("|                   |");
-           Console.WriteLine("|     hello world   |");
-           Console.WriteLine("|                   |");
-           Console.WriteLine("|                   |");
-           Console.WriteLine("|                   |");
-           Console.WriteLine("|                   |");
-           Console.WriteLine("|___________________|");
-           Console.WriteLine("|                   |");
-           Console.WriteLine("|___________________|");
            
-            inicio:
-           Console.WriteLine("Digite MENSAGEM para enviar uma mensagem");
-           Console.WriteLine("Digite LIGAR para uma ligaçao");
-           Console.WriteLine("Digite DESLIGAR para desligar o celular");
-           switch (Console.ReadLine())
-           {
-               case "MENSAGEM":
-                    Console.WriteLine("Digite uma mensagem");
-                    Cell.Mensagem(Console.ReadLine());
-                    goto inicio;
-                    
-                    break;
-                case "LIGAR":
-                    Console.WriteLine("Digite o numero para ligaçao");
-                    Cell.Ligar(Console.ReadLine());
-                    goto inicio;
-                    
-                    break;
-                case "DESLIGAR":
-                    Cell.Desligar();
-                    
-                    break;
-               default:
-                    Console.WriteLine("Comando invalido");
-                    Console.Clear();
-                    goto inicio;
-                    break;    
-           }
+          
+           
+           
+           
            
            
 
